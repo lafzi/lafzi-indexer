f9e7e301bbde72c699bc1580ddaa4e43865f7e97
abrari <muhammad.abrari@gmail.com>
2017-04-13 09:18:58 +0700

    Hapus fungsi tak terpakai

--- lib/trigram.php
+++ lib/trigram.php
@@ -23,39 +23,6 @@ function ekstrak_trigram($string) {
     
 }
 
-// menghitung frekuensi trigram
-// param  : $string yang akan dihitung frekuensi trigramnya
-// return : array berisi trigram sebagai key dan frekuensi sebagai value
-function frekuensi_trigram_string($string) {
-    
-    $array = ekstrak_trigram($string);
-    
-    // fungsi bawaan PHP
-    return array_count_values($array);
-    
-}
-
-// [UNUSED]
-// menghitung frekuensi trigram
-// param  : $string yang akan dihitung frekuensi dan posisi kemunculan pertama trigramnya
-// return : array berisi trigram sebagai key dan (frekuensi, posisi) sebagai value
-function trigram_frekuensi_posisi($string) {
-    
-    $array = ekstrak_trigram($string);
-    $array_freq = array_count_values($array);
-    
-    $res = array();
-    
-    foreach ($array_freq as $trigram => $freq) {
-        
-        $pos = strpos($string, $trigram) + 1;
-        $res[$trigram] = array($freq, $pos);
-        
-    }
-    
-    return $res;
-    
-}
 
 // menghitung frekuensi trigram
 // param  : $string yang akan dihitung frekuensi dan seluruh posisi kemunculan trigramnya
